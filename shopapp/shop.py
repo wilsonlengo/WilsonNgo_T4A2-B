4479a00da47efafe0558a97cfa518e8357be971d
wilsonlengo <wilsonngo0911@gmail.com>
2022-03-08 17:56:28 +1100

    market page created

--- shopapp/shop.py
+++ shopapp/shop.py
@@ -2,6 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home_page():
     return render_template("home.html")
+
+@app.route("/market")
+def market_page():
+    items = [
+    {'id': 1, 'name': 'Phone', 'description': 'It is a phone', 'price': 500},
+    {'id': 2, 'name': 'Laptop', 'description': 'It is a laptop', 'price': 900},
+    {'id': 3, 'name': 'Keyboard', 'description': 'It is a keyboard', 'price': 150}
+    ]
+    return render_template("market.html", items=items)
+
 
