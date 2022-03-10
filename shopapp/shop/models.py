3be196c42c69be45f84470a8f7095d97b2e2f803
wilsonlengo <wilsonngo0911@gmail.com>
2022-03-10 12:45:52 +1100

    split into packages

---
+++ shopapp/shop/models.py
@@ -0,0 +1,11 @@
+from shop import db
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    description = db.Column(db.String(length=1500), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"Item {self.name}"
