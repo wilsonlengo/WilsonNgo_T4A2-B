36adf259486701ebf92baa8bc82f09bb7ef424a0
wilsonlengo <wilsonngo0911@gmail.com>
2022-03-11 12:42:44 +1100

    added flash messages, validators

--- shopapp/shop/__init__.py
+++ shopapp/shop/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '44cb08adee3c79c23ce4583c'
--- shopapp/shop/routes.py
+++ shopapp/shop/routes.py
@@ -1,7 +1,8 @@
 from shop import app
-from flask import render_template
-from shop.models import Item
+from flask import render_template, redirect, url_for, flash
+from shop.models import Item, User
 from shop.forms import RegisterForm
+from shop import db
 
 @app.route("/")
 @app.route("/home")
@@ -14,7 +15,18 @@ def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
 
-@app.route("/register")
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
-    return render_template("register.html", form=form)
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}: #If there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category="danger")
+
+    return render_template('register.html', form=form)
