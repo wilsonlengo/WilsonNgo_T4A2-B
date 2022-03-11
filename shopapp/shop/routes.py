c81d1994421e23f25321c963b325ef5aa5c2b1f4
wilsonlengo <wilsonngo0911@gmail.com>
2022-03-12 00:28:55 +1100

    added user auth, custom login and logout, reformatted home page

--- shopapp/shop/routes.py
+++ shopapp/shop/routes.py
@@ -1,8 +1,9 @@
 from shop import app
 from flask import render_template, redirect, url_for, flash
 from shop.models import Item, User
-from shop.forms import RegisterForm
+from shop.forms import RegisterForm, LoginForm
 from shop import db
+from flask_login import login_user, logout_user, login_required
 
 @app.route("/")
 @app.route("/home")
@@ -11,6 +12,7 @@ def home_page():
 
 
 @app.route("/market")
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
@@ -21,12 +23,36 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
-                              password_hash=form.password1.data)
+                              password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('market_page'))
-    if form.errors != {}: #If there are not errors from the validations
+    if form.errors != {}: # If there are not errors from the validations
         for err_msg in form.errors.values():
-            flash(f'There was an error with creating a user: {err_msg}', category="danger")
+            flash(f'There was an error with creating a user: {err_msg}', category="danger") # Depecting error when fed wrong information
 
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit(): # Validating all information upon submitting information
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction( # Calling upon the function set in models.py
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success') # Depicting a successful login message
+            return redirect(url_for('market_page')) # Redirecting user to market page upon successful login
+        else:
+            flash('Username and password are not match! Please try again', category='danger') # Depicting an invalid login
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info') # Notifies user that they have successfully logged out
+    return redirect(url_for("home_page"))
